6dec4b90e8d31a2985b75d081cd360decf91003f
Aneenaaaa <aneenaali123@gmail.com>
2024-04-28 00:10:45 +0530

    deleted unwanted files

--- Untitled-1.c
+++
@@ -1,8 +0,0 @@
-prog_char dictEnglishSalami[] PROGMEM = "Salami";
-prog_char dictEnglishProsecco[] PROGMEM = "Prosecco";
-const char *string_table[] = {
-   dictEnglishSalami,
-   dictEnglishProsecco,
-   ...
-   };
-Print(string_table[0][0])
