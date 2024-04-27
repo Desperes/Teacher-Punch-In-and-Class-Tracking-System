2e0d23a64e9b68f6c3fdb5def80f5c4174173315
Aneenaaaa <aneenaali123@gmail.com>
2024-04-27 23:48:27 +0530

    optimised and byte datatype implemented

--- main_program.ino
+++ main_program.ino
@@ -21,21 +21,12 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 SoftwareSerial mySerial(FINGERPRINT_RX_PIN, FINGERPRINT_TX_PIN);
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 volatile bool InterruptFlag = false;
-<<<<<<< Updated upstream:main_program/main_program.ino
-const char PROGMEM subject[][8] = {
-    "MONDAY", "EL", "VL", "VL", "EM", "IT", "HO",
-    "TUESDAY", "VL", "IT", "EL", "MP", "MP", "MP",
-    "WEDNESDAY", "EL", "ME", "VL", "EM", "CW", "HO",
-    "THURSDAY", "ME", "EM", "IT", "MP", "MP", "MP",
-    "FRIDAY", "IT", "EM", "ME", "HO", "MI", "MI"
-=======
 const PROGMEM char subject[][7][5] = {
     {"MONDAY", "EL", "VL", "VL", "EM", "IT", "HO"},
     {"TUESDAY", "VL", "IT", "EL", "MP", "MP", "MP"},
     {"WEDNESDAY", "EL", "ME", "VL", "EM", "CW", "HO"},
     {"THURSDAY", "ME", "EM", "IT", "MP", "MP", "MP"},
     {"FRIDAY", "IT", "EM", "ME", "HO", "MI", "MI"}
->>>>>>> Stashed changes:main_program.ino
 };
 
 // const char subject[][11] PROGMEM= {
@@ -52,25 +43,11 @@ const byte mapping[5][6] PROGMEM= {
     {1, 4, 5, 6, 7, 5},
     {4, 5, 6, 2, 1, 5}
 };
-<<<<<<< Updated upstream:main_program/main_program.ino
-char attendance[5][6][2];
-=======
 byte attendance[5][6];
->>>>>>> Stashed changes:main_program.ino
 RTC_DS1307 rtc;
 byte currentDay = rtc.now().dayOfTheWeek();
 void setup() {
   Serial.begin(9600);
-<<<<<<< Updated upstream:main_program/main_program.ino
-  Serial.println("HI");
-  for (byte i = 0; i < 5; i++)
-  {
-    for (byte j = 0; j < 6; j++)
-    {
-        attendance[i][j][0] = '\0'; // Null-terminate the string
-    }
-  }
-=======
   // for (int i = 0; i < 5; i++)
   // {
   //   for (int j = 0; j < 6; j++)
@@ -78,7 +55,6 @@ void setup() {
   //       attendance[i][j][0] = '\0'; // Null-terminate the string
   //   }
   // }
->>>>>>> Stashed changes:main_program.ino
   pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
   pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
@@ -102,16 +78,6 @@ void setup() {
   Serial.println(F("\n\nAdafruit finger detect test"));
 
   finger.begin(57600);
-<<<<<<< Updated upstream:main_program/main_program.ino
-  delay(5);
-  if (finger.verifyPassword())
-  {
-    Serial.println("Found fingerprint sensor!");
-  }
-  else
-  {
-    Serial.println("Did not find fingerprint sensor :(");
-=======
   delay(3);
   if (finger.verifyPassword())
   {
@@ -120,7 +86,6 @@ void setup() {
   else
   {
     Serial.println(F("Did not find fingerprint sensor :("));
->>>>>>> Stashed changes:main_program.ino
     while (1)
     {
       delay(1);
@@ -137,40 +102,23 @@ void loop() {
         byte day = rtc.now().dayOfTheWeek();
         byte hour= rtc.now().hour();
         byte mappingVal;
-<<<<<<< Updated upstream:main_program/main_program.ino
-
-        if (day == 0 || day == 6) 
-=======
         if (day == 1 || day == 7) 
->>>>>>> Stashed changes:main_program.ino
           return 0;    // if the day is sat or sunday come out of the if condition
 
         if (hour < 12) {  
             mappingVal= pgm_read_word(&(mapping[day-2][hour-9]));                    // excluding 12pm
             if (checkFingerprintID()== mappingVal) {
-<<<<<<< Updated upstream:main_program/main_program.ino
-                strcpy(attendance[day-1][hour-9], "p");
-            } else {
-                strcpy(attendance[day-1][hour-9], "a");
-=======
                 attendance[day-2][hour-9]=1;
             } else {
                 attendance[day-2][hour-9]=0;
->>>>>>> Stashed changes:main_program.ino
 
             }
         } else if (hour > 12) {
             mappingVal= pgm_read_word(&(mapping[day-2][hour-10])); 
             if (checkFingerprintID()== mappingVal) {
-<<<<<<< Updated upstream:main_program/main_program.ino
-                strcpy(attendance[day-1][hour-10], "p");
-            } else {
-                strcpy(attendance[day-1][hour-10], "a");
-=======
                 attendance[day-2][hour-10]=1;
             } else {
                 attendance[day-2][hour-10]=0;
->>>>>>> Stashed changes:main_program.ino
             }
         }    
     InterruptFlag=false;
@@ -182,11 +130,7 @@ void isr()
 {
   InterruptFlag= true;
 }
-<<<<<<< Updated upstream:main_program/main_program.ino
-void DisplayTable(const char* subject[],  char attendance[][6][2], int day)
-=======
 void DisplayTable(const char* subject[], byte attendance[][6], byte day)
->>>>>>> Stashed changes:main_program.ino
 {
   display.clearDisplay();
   //draw arrows 
@@ -201,15 +145,6 @@ void DisplayTable(const char* subject[], byte attendance[][6], byte day)
   byte rectWidth = 42; // Adjusted width to fit within the screen
   byte rectHeight = 27; // Adjusted height to fit within the screen
   byte count=1;
-<<<<<<< Updated upstream:main_program/main_program.ino
-  for (byte row = 0; row < 2; row++)
-  {
-     for (byte col = 0; col < 3; col++)
-    {
-      byte x = col * (rectWidth);
-      byte y = row * (rectHeight);
-      
-=======
   display.setTextSize(1);  // Set text size
   display.setTextColor(WHITE);
   char buffer[10];
@@ -219,19 +154,11 @@ void DisplayTable(const char* subject[], byte attendance[][6], byte day)
     {
       byte x = col * (rectWidth);
       byte y = row * (rectHeight);
->>>>>>> Stashed changes:main_program.ino
       display.drawRect(x, y, rectWidth, rectHeight, WHITE);
 
       // Add tick mark pattern and 'EM' inside one rectangle
       // centerX = x + rectWidth / 2;
       // centerY = y + rectHeight / 2;
-<<<<<<< Updated upstream:main_program/main_program.ino
-        display.setTextSize(1);  // Set text size
-        display.setTextColor(WHITE); // Set text color
-        display.setCursor(x + rectWidth - 13, y + rectHeight - 10);     
-        display.print(subject[day-2][count]);        
-        if(attendance[day-2][count-1]=="p")
-=======
         
          // Set text color
         display.setCursor(x + rectWidth - 13, y + rectHeight - 10);
@@ -240,16 +167,11 @@ void DisplayTable(const char* subject[], byte attendance[][6], byte day)
         display.print(buffer);
         if(row<2 && col<3){   
         if(attendance[day-2][count-1]==1)
->>>>>>> Stashed changes:main_program.ino
         {
         display.drawLine(x + 5, y + 10, x + 10, y + 20, WHITE);
         display.drawLine(x + 10, y + 20, x + 30, y + 5, WHITE);
         }
-<<<<<<< Updated upstream:main_program/main_program.ino
-        else if(attendance[day-2][count-1]=="a")
-=======
         else if(attendance[day-2][count-1]==0)
->>>>>>> Stashed changes:main_program.ino
         {
         display.drawLine(x + 5, y + 4, x + 22, y + 20, WHITE);
         display.drawLine(x + 5, y + 20, x + 22, y + 4, WHITE);
@@ -310,10 +232,7 @@ void leftButtonISR()
     }
     // Redraw the schedule for the new day
     DisplayTable((const char **)subject,attendance,currentDay);
-<<<<<<< Updated upstream:main_program/main_program.ino
-=======
       delay(50); // Debounce delay
->>>>>>> Stashed changes:main_program.ino
   // Code to execute when the left button is pressed
 
 }
@@ -326,13 +245,8 @@ void rightButtonISR()
     }
     // Redraw the schedule for the new day
     DisplayTable((const char **)subject,attendance,currentDay);
-<<<<<<< Updated upstream:main_program/main_program.ino
- 
-}
-=======
     delay(50);
 }
 // void readString(int row, int col, char *buffer) {
 //   strcpy_P(buffer, (char*)pgm_read_word(&(subject[row][col])));
 // }
->>>>>>> Stashed changes:main_program.ino
